aea91d5898ef34732f9e3a8ee94ba16f7cc10880
OTZYDK <amatulraheem.s50@gmail.com>
2022-05-15 21:49:27 -0500

    add three files

---
+++ Final_Exam_ISI_CS_2022.js
@@ -0,0 +1,9 @@
+
+const text = document.createElement("div");
+text.setAttribute("id", "new-div");
+text.innerText = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi, provident! Nulla excepturi rem sapiente? Sed doloremque, quaerat sapiente quam sequi deleniti ratione, excepturi ex amet asperiores blanditiis eius ullam dicta.";
+document.body.appendChild(text);
+
+const head = document.getElementById("my-final");
+head.innerText = "Hello ISI, I am done!";
+
